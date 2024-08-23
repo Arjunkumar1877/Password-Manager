9acf41a8c8e49f91265c6cdad376c327c69352de
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-24 02:27:29 +0530

    initial commit

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import bgGround from '../assets/bg.png';
+
+const Login: React.FC = () => {
+
+    console.log(bgGround)
+  return (
+    <div className='max-w-screen max-h-screen '>
+      <img src={bgGround} alt="bg" className='h-full w-full fixed' />
+
+      <div className="relative ">
+       
+       <div className="flex flex-col w-">
+
+       </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
